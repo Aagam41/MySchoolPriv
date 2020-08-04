8f9522a8eebde6c5c9d39c0ba49e89063f61f32e
Aagam41 <aagam.h.sheth@icloud.com>
2020-08-04 23:52:37 +0530

    Added KNN Query with raw data from Database
    Signed-off-by: Aagam41 <aagam.h.sheth@icloud.com>

--- MySchool/StudentPerformance/views.py
+++ MySchool/StudentPerformance/views.py
@@ -1,14 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def test(request):
-    return 1
-
-
-def test123(request):
-    return 1
-def test678(request):
-    return 3
-
-def test1234567():
-    return 234
