3b2bd3e48d21e8e9e3743e8e3daf6b5bf07f6f3b
Aagam41 <aagam.h.sheth@icloud.com>
2020-12-13 14:19:41 +0530

    committing for cherry pick
    Signed-off-by: Aagam41 <aagam.h.sheth@icloud.com>

--- MySchoolSource/StudentPerformance/urls.py
+++ MySchoolSource/StudentPerformance/urls.py
@@ -2,24 +2,25 @@ from django.urls import path
 
 from . import views
 
+from aagam_packages.django_model_extensions.views import generic
+
 app_name = 'StudentPerformance'
 
 urlpatterns = [
     path('paper-type-form/list', views.PaperTypeListView.as_view(), name='papertype_list_view'),
-    path('paper-type-form/create', views.PaperTypeCreateView.as_view(), name='papertype_create_view'),
     path('paper-type-form/update/<int:pk>', views.PaperTypeUpdateView.as_view(), name='papertype_update_view'),
-
     path('paper-entry-form/list', views.PaperEntryListView.as_view(), name='paperentry_list_view'),
-    path('paper-entry-form/create', views.PaperEntryCreateView.as_view(), name='paperentry_create_view'),
     path('paper-entry-form/update/<int:pk>', views.PaperEntryUpdateView.as_view(), name='paperentry_update_view'),
 
-    path('paper-entry-form/create1/<str:app_label>/<str:model_label>',
-         views.ModelObjectCreateView.as_view(), name='model-object_create_view'),
+    path('create/<str:app_label>/<str:model_label>',
+         generic.ModelObjectCreateView.as_view(), name='model-object_create_view'),
+    path('create/<str:app_label>/<str:model_label>/<str:template_label>/',
+         generic.ModelObjectCreateView.as_view(), name='model-object_create_view'),
 
-    path('paper-entry-form/delete1/<str:app_label>/<str:model_label>/<int:pk>',
-         views.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
-    path('paper-entry-form/delete1/<str:app_label>/<str:model_label>/<int:pk>/<str:template_label>',
-         views.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
-    path('paper-entry-form/delete1/<str:app_label>/<str:model_label>/<int:pk>/<str:template_label>/<str:success_label>',
-         views.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
+    path('delete/<str:app_label>/<str:model_label>/<int:pk>',
+         generic.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
+    path('delete/<str:app_label>/<str:model_label>/<int:pk>/<str:template_label>',
+         generic.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
+    path('delete/<str:app_label>/<str:model_label>/<int:pk>/<str:template_label>/<str:success_label>',
+         generic.ModelObjectDeleteView.as_view(), name='model-object_delete_view'),
 ]
--- MySchoolSource/StudentPerformance/views.py
+++ MySchoolSource/StudentPerformance/views.py
@@ -21,11 +21,6 @@ class PaperTypeListView(ListView):
         return context
 
 
-class PaperTypeCreateView(CreateView):
-    model = PaperType
-    fields = ['paper_type', 'out_of']
-
-
 class PaperTypeUpdateView(UpdateView):
     model = PaperType
     fields = ['paper_type', 'out_of']
@@ -40,109 +35,6 @@ class PaperEntryListView(ListView):
         return context
 
 
-class PaperEntryCreateView(CreateView):
-    model = PaperEntry
-    fields = ['paper_entry_name', 'subject', 'paper_type', 'paper_entry_date']
-
-
 class PaperEntryUpdateView(UpdateView):
     model = PaperEntry
     fields = ['paper_entry_name', 'subject', 'paper_type', 'paper_entry_date']
-
-
-class ModelObjectCreateView(CreateView):
-    def __init__(self):
-        super(ModelObjectCreateView, self).__init__()
-        self.app_label = ""
-        self.model_label = ""
-        self.template_label = ""
-        self.success_label = ""
-
-    def dispatch(self, request, *args, **kwargs):
-        self.model_label = kwargs.get('model_label', None)
-        self.app_label = kwargs.get('app_label', None)
-        self.model = apps.get_model(self.app_label, self.model_label.capitalize())
-        self.fields = self.get_modelobject_fields_label()
-
-        self.context_object_name = 'object'
-
-        self.template_label = kwargs.get('template_label', None)
-        self.template_name = self.get_template_name_label()
-        try:
-            ret = super(ModelObjectCreateView, self).dispatch(request, *args, **kwargs)
-        except AttributeError:
-            raise Http404
-        return ret
-
-    def get_template_name_label(self):
-        if self.template_label == "0":
-            template_name = f'{self.app_label}/{self.model_label}_create.html'
-        elif self.template_label:
-            template_name = self.template_name
-        else:
-            template_name = 'modelobject_create.html'
-        return template_name
-
-    def get_modelobject_fields_label(self):
-        model_label = list()
-        field_label = list()
-        for app in apps.get_app_configs():
-            for model in app.get_models():
-                model_label.append(model._meta.verbose_name.replace(" ", ""))
-        field_labels = self.model._meta.get_fields()
-        for field in field_labels:
-            if field.name == f'{self.model.objects.model._meta.db_table}_id':
-                pass
-            elif field.name in model_label:
-                pass
-            else:
-                field_label.append(field.name)
-        return field_label
-
-
-class ModelObjectDeleteView(DeleteView):
-    def __init__(self):
-        super(ModelObjectDeleteView, self).__init__()
-        self.app_label = ""
-        self.model_label = ""
-        self.template_label = ""
-        self.success_label = ""
-
-    def dispatch(self, request, *args, **kwargs):
-        self.model_label = kwargs.get('model_label', None)
-        self.app_label = kwargs.get('app_label', None)
-        self.model = apps.get_model(self.app_label, self.model_label.capitalize())
-
-        self.context_object_name = f'object'
-
-        self.template_label = kwargs.get('template_label', None)
-        self.template_name = self.get_template_name_label()
-
-        self.success_label = kwargs.get('success_label', None)
-        self.success_url = reverse_lazy(self.get_success_url_label())
-        try:
-            ret = super(ModelObjectDeleteView, self).dispatch(request, *args, **kwargs)
-        except AttributeError:
-            raise Http404
-        except TemplateDoesNotExist:
-            return HttpResponse(status=501)
-        return ret
-
-    def get_template_name_label(self):
-        if self.template_label == "0":
-            template_name = f'{self.app_label}/{self.model_label}_confirm_delete.html'
-        elif self.template_label:
-            template_name = self.template_name
-        else:
-            template_name = 'modelobject_confirm_delete.html'
-        return template_name
-
-    def get_success_url_label(self):
-        if self.success_label == "0":
-            success_url = f'{self.app_label}/{self.model_label}_success_url.html'
-        elif self.success_label:
-            yoda_saberize_print(self.success_label, YodaSaberColor.WHITE, YodaSaberColor.RED)
-            success_url = self.success_label
-        else:
-            success_url = 'MySchoolHome:test'
-        return success_url
