b46fab08b7a703446b8774d78a8680c3c31eccbc
Aagam41 <aagam.h.sheth@icloud.com>
2020-12-19 08:23:09 +0530

    commited last changes
    Signed-off-by: Aagam41 <aagam.h.sheth@icloud.com>

--- MySchoolSource/StudentFeedback/views.py
+++ MySchoolSource/StudentFeedback/views.py
@@ -16,39 +16,39 @@ def edit_user(request):
     return render(request, 'notNeeded/user_update.html')
 
 def add_class(request):
-    return render(request, 'StudentPerformance/standard_create.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/standard_create.html')
 
 def class_detail(request):
     yoda_saberize_print("Aagam", YodaSaberColor.WHITE, YodaSaberColor.HOTPINK)
     data = ap.StandardSection.objects.values('standard', 'section')
-    return render(request, 'StudentPerformance/standardsection_list.html', {'da': data})
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/standardsection_list.html', {'da': data})
 
 def edit_class(request):
     return render(request, 'notNeeded/standard_update.html')
 
 def subject_detail(request):
-    return render(request, 'StudentPerformance/tblsubject_list.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/tblsubject_list.html')
 
 def add_subject(request):
-    return render(request, 'StudentPerformance/tblsubject_create.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/tblsubject_create.html')
 
 def edit_subject(request):
     return render(request, 'notNeeded/tblsubject_update.html')
 
 def chapter_detail(request):
-    return render(request, 'StudentPerformance/subjectchapter_list.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/subjectchapter_list.html')
 
 def add_chapter(request):
-    return render(request, 'StudentPerformance/subjectchapter_create.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/subjectchapter_create.html')
 
 def edit_chapter(request):
-    return render(request, 'notNeeded/subjectchapter_update.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/subjectchapter_update.html')
 
 def topic_detail(request):
-    return render(request, 'StudentPerformance/chaptertopic_list.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/chaptertopic_list.html')
 
 def add_topic(request):
-    return render(request, 'StudentPerformance/chaptertopic_create.html')
+    return render(request, 'StudentPerformance/../zzzbrijtemplates/chaptertopic_create.html')
 
 def edit_topic(request):
     return render(request, 'notNeeded/chaptertopic_update.html')
