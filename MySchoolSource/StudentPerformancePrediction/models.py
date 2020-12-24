eae61b29cac399eb5d43be02d7b60ad9f0a93db6
Aagam41 <aagam.h.sheth@icloud.com>
2020-12-24 10:27:34 +0530

    changed database for predictions
    Signed-off-by: Aagam41 <aagam.h.sheth@icloud.com>

--- MySchoolSource/StudentPerformancePrediction/models.py
+++ MySchoolSource/StudentPerformancePrediction/models.py
@@ -23,8 +23,7 @@ class StudentEfficacy(amdl.AagamBaseModel):
     student = models.ForeignKey(MySchoolUser, models.DO_NOTHING)
     past_marks = models.IntegerField()
     past_marks1 = models.IntegerField()
-    predictions = models.IntegerField()
-    group = models.IntegerField()
+    predictons = models.IntegerField()
 
     class Meta:
         managed = True
